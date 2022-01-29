9ac27b963185f8995cd229e300e54e8570f92663
Armin Pribbernow <armpribb@posteo.de>
2022-01-30 00:55:16 +0100

    add clipboard support for unix (x11)

--- src/clipboard_impl_unix.cpp
+++ src/clipboard_impl_unix.cpp
@@ -1,7 +1,9 @@
 #include "clipboard_impl.h"
 
+#include <clip.h>
+
 namespace output {
 void clipboard_impl::copy_to_clipboard(const std::string &str) const {
-  (void)str;
+    clip::set_text(str);
 }
 } // namespace output
