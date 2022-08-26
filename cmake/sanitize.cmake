0dcd152089db20238fbe4baa39c659a67f1042ae
Armin Pribbernow <github@armpribb.com>
2022-08-26 02:14:10 +0200

    fix sanitizer link options (gcc/clang)

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -3,7 +3,7 @@ if(MSVC)
     set(CMAKE_CXX_FLAGS_SANITIZE "/Zi /O1 /DEBUG" CACHE STRING "Flags used by the CXX compiler during SANITIZE builds." FORCE)
     set(CMAKE_EXE_LINKER_FLAGS_SANITIZE "/debug /INCREMENTAL:NO" CACHE STRING "Flags used by the linker during SANITIZE builds." FORCE)
 else()
-    set(CMAKE_CXX_FLAGS_SANITIZE "-g -O1" CACHE STRING "Flags used by the CXX compiler during Sanitizer builds." FORCE)
+    set(CMAKE_CXX_FLAGS_SANITIZE "-O1 -g" CACHE STRING "Flags used by the CXX compiler during Sanitizer builds." FORCE)
 endif()
 
 if(NOT CMAKE_BUILD_TYPE)
