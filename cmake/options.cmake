e9613e6fa74eddf92bd3b4f54a1cfba066e22a92
Armin Pribbernow <github@armpribb.com>
2022-10-01 12:16:10 +0200

    fix mac cmake preset and compile options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,6 +2,6 @@ include(CMakeDependentOption)
 
 option(RAW_DEBUG_INFO "Build RAW with debug info" ON)
 
-cmake_dependent_option(RAW_RECORD_COMPILE_SWITCHES "Record compile switches" ON "NOT MSVC" OFF)
+cmake_dependent_option(RAW_RECORD_COMPILE_SWITCHES "Record compile switches" ON "GCC" OFF)
 
 set(RAW_SANITIZER_TYPE "ASAN" CACHE STRING "Specifiy Sanitizer to use: ASAN, UBSAN, LSAN, MSAN, TSAN")
